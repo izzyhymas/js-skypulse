4fccbbeaf826513935fa6e4afc93b61372f06b37
Izzy Hymas <izzyhymas@gmail.com>
2024-06-21 08:02:14 -0600

    Create function for graph

--- script.js
+++ script.js
@@ -25,12 +25,33 @@ document.addEventListener("DOMContentLoaded", async function () {
     const forecast = data2["properties"]["periods"];
 
     const hourlyForecast = [];
+
     for (let i = 0; i < 24; i++) {
       const getHourlyForecast = forecast[i]["temperature"];
       hourlyForecast.push(getHourlyForecast);
     }
-      console.log(`24 Hour Forecast: ${hourlyForecast}`);
-      document.getElementById("hourly-weather").innerHTML = hourlyForecast;
+    console.log(`24 Hour Forecast: ${hourlyForecast}`);
+    document.getElementById("hourly-weather").innerHTML = hourlyForecast;
+  };
+
+  // Weekly Forecast
+const days = [];
+const temp = [];
+  const getWeeklyForecast = async () => {
+    const weeklyForecast = await fetch(
+      "https://api.weather.gov/gridpoints/SLC/20,19/forecast"
+    );
+    const data4 = await weeklyForecast.json();
+    const forecast = data4["properties"]["periods"];
+
+    for (let i = 0; i < forecast.length; i++) {
+      const getDay = forecast[i]["name"];
+      const getTemp = forecast[i]["temperature"];
+      days.push(getDay);
+      temp.push(getTemp);
+    }
+    console.log(days);
+    console.log(temp);
   };
 
   // Alerts
@@ -45,7 +66,33 @@ document.addEventListener("DOMContentLoaded", async function () {
     document.getElementById("weather-alert").innerHTML = alert;
   };
 
+
+  const ctx = document.getElementById("myChart");
+  const data = [
+    { day: days, temp: temp },
+    { day: days, temp: temp },
+    { day: 2012, temp: 15 },
+    { day: 2013, temp: 25 },
+    { day: 2014, temp: 22 },
+    { day: 2015, temp: 30 },
+    { day: 2016, temp: 28 },
+  ];
+
+  new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: data.map((row) => row.day),
+      datasets: [
+        {
+          label: "Daily Temperature",
+          data: data.map((row) => row.temp),
+        },
+      ],
+    },
+  });
+
   getCurrentForecast();
   get24HourForecast();
   getWeatherAlert();
+  getWeeklyForecast();
 });
